6fee6f49ad99ff7ce8b6a3a3bf9606cebd258f70
CameronLofy <cameron.lofy@sjsu.edu>
2018-02-25 23:20:49 -0800

    I don't even know tbh

--- lib/lab04/src/calculator.cpp
+++ lib/lab04/src/calculator.cpp
@@ -1,13 +1,25 @@
 
 
 #include <string>
+#include <cstring>
 #include "calculator.h"
 namespace lab4 {
-    void calculator::parse_to_infix(std::string &input_expression) {
+    void calculator::parse_to_infix(std::string &input_expression) {        //TODO:: clean white spaces,
+        lab1::expressionstream stream(input_expression);
+        
+
 
     }
 
     void calculator::convert_to_postfix(lab3::fifo infix_expression) {
+        lab3::lifo stack;
+
+        for(int i=0; i<infix_expression.size(); i++){
+            int get_number(std::string infix_expression[i]);
+
+        }
+
+        //put everything here regarding queue/stack and auxiliary functions
 
     }
 
@@ -16,6 +28,8 @@ namespace lab4 {
     }
 
     calculator::calculator(std::string &input_expression) {
+        parse_to_infix(input_expression);
+        convert_to_postfix(infix_expression);
 
     }
 
@@ -33,13 +47,53 @@ namespace lab4 {
 
 
     // AUXILIARY FUNCTIONS
-    bool is_number(std::string input_string);
+    bool is_number(std::string input_string){
+        if(input_string == "0"||"1"||"2"||"3"||"4"||"5"||"6"||"7"||"8"||"9"){
+            return true;
+        }
 
-    bool is_operator(std::string input_string);
+    }
 
-    int get_number(std::string input_string);
 
-    std::string get_operator(std::string input_string);
+    bool is_operator(std::string input_string){
+        if(input_string == "+"|| "-"|| "*"|| "/"|| "("||")"||"^"){
+            return true;
+        }
 
-    int operator_priority(std::string operator_in);
+
+    }
+
+    int get_number(std::string input_string) {
+        if(is_number(input_string)){
+            lab3::fifo enqueue(input_string);
+        }
+        else if (is_operator(input_string)){
+            lab3::lifo push(input_string);
+        }
+        else {
+            throw"ERROR: NOT OPERATOR OR NUMBER";
+        }
+    }
+
+    std::string get_operator(std::string input_string){
+        lab3::lifo pop(input_string);
+
+    }
+
+    int operator_priority(std::string operator_in){
+        int priority;
+        if(operator_in == "+"||"-"){
+            priority = 1;
+        }
+        if(operator_in == "*" || "/"){
+            priority = 2;
+        }
+        if(operator_in == "^"){
+            priority = 3;
+        }
+        if(operator_in == "("|| ")"){
+            priority = 4;
+        }
+        return priority;
+    }
 }
