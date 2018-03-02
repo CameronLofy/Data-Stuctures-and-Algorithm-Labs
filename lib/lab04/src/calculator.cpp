b7bed02333eaeb474117a843b112f79c0bbce82b
CameronLofy <cameron.lofy@sjsu.edu>
2018-03-01 22:23:40 -0800

    Calculator working

--- lib/lab04/src/calculator.cpp
+++ lib/lab04/src/calculator.cpp
@@ -24,19 +24,14 @@ namespace lab4 {
         while (!infix_expression.is_empty()) {
             current_token = infix_expression.top();
             infix_expression.dequeue();
-            if(infix_expression.is_empty()) {
-                while (!stack.is_empty()) {
-                    std::string temp = stack.top();
-                    stack.pop();
-                    postfix_expression.enqueue(temp);
-                }
-            }
+
             if (is_number(current_token) == true){
                 postfix_expression.enqueue(current_token);
             }
+
             if (is_operator(current_token)){
                 while (!stack.is_empty() && operator_priority(current_token) <= operator_priority(stack.top()) &&
-                       current_token != "(") {
+                       stack.top() != "(") {
                     postfix_expression.enqueue(stack.top());
                     stack.pop();
                 }
@@ -51,69 +46,17 @@ namespace lab4 {
                     postfix_expression.enqueue(stack.top());
                     stack.pop();
                 }
+                stack.pop();
             }
         }
 
-        while (!stack.is_empty()) {
-            postfix_expression.enqueue(current_token);
+        while (!stack.is_empty()) {     //puts everything left from stack to queue
+            if(stack.top()=="(") {      //skips the '(' within the stack
+                stack.pop();
+            }
+            postfix_expression.enqueue(stack.top());
             stack.pop();
         }
-
-        /*
-        int size = infix_expression.size();
-        std::string temp[size];
-        for (int i = 0; i < size; i++) {       //copies infix expression into string array
-            temp[i] = infix_expression.top();
-            infix_expression.dequeue();
-        }
-        int counter =0;
-        lab1::expressionstream post(temp[size]);    //debugger stops here
-        post.get_current_token();
-        while(counter < size) {
-            if (post.next_token_is_int()) {
-                postfix_expression.enqueue(temp[counter]);
-            }
-            if (post.next_token_is_op()) {
-                if (stack.is_empty()) {
-                    stack.push(temp[counter]);
-                }
-
-                else if(operator_priority(stack.top()) >= operator_priority(temp[counter])){
-                    while(!stack.is_empty()){
-                        postfix_expression.enqueue(stack.top());
-                        stack.pop();
-                    }
-                }
-
-                else if(operator_priority(stack.top()) < operator_priority(temp[counter])){
-                    stack.push(temp[counter]);
-                }
-
-            }
-
-            if(post.next_token_is_paren_open()){
-                stack.push(temp[counter]);
-            }
-
-            if(post.next_token_is_paren_close()){
-                while(stack.top() != ")") {
-                    while (!stack.is_empty()) {
-                        std::string tempOP = stack.top();
-                        postfix_expression.enqueue(tempOP);
-                        stack.pop();
-                    }
-                }
-            }
-            post.get_next_token();
-            ++counter;
-            if(counter>size){
-                while(!stack.is_empty()){
-                    postfix_expression.enqueue(stack.top());
-                    stack.pop();
-                }
-            }
-        }
-         */
     }
 
     calculator::calculator() {
@@ -159,49 +102,34 @@ namespace lab4 {
                 calc_stack.pop();
 
                 if(tempOP == "+"){
-                    final = x+y;
+                    final = y+x;
                     std::string calc = std::to_string(final);
                     calc_stack.push(calc);
+                }
 
-        }
+                if(tempOP == "-"){
+                    final = y-x;
+                    std::string calc = std::to_string(final);
+                    calc_stack.push(calc);
+                }
 
+                if(tempOP == "*"){
+                    final = y*x;
+                    std::string calc = std::to_string(final);
+                    calc_stack.push(calc);
+                }
 
-
+                if(tempOP == "/"){
+                    final = y/x;
+                    std::string calc = std::to_string(final);
+                    calc_stack.push(calc);
+                }
 
             }
         }
-        return 0;
+        int final_calc = std::stoi(calc_stack.top());
+        return final_calc;
 
-
-        /*
-        bool is_operator(std::string input_string);
-        int size = postfix_expression.size();
-        std::string temp[size];
-        lab3::fifo final_calc;
-        for (int i = 0; i < size; i++) {
-            temp[i] = postfix_expression.top();
-            postfix_expression.dequeue();
-            if(!is_operator(temp[i])){
-                final_calc.enqueue(temp[i]);
-            }
-            int temp1 = std::stoi(final_calc.top());
-            final_calc.dequeue();
-            int temp2 = std::stoi(final_calc.top());
-            final_calc.dequeue();
-            if (temp[i] == "+") {
-                return temp1 + temp2;
-            } else if (temp[i] == "-") {
-                return temp1 - temp2;
-            } else if (temp[i] == "*") {
-                return temp1 * temp2;
-            } else if (temp[i] == "/") {
-                return temp1 / temp2;
-            }
-        }
-
-
-        return 0;
-         */
     }
 
     std::ostream &operator<<(std::ostream &stream, calculator &RHS) {
--- test/lab04_tests.cpp
+++ test/lab04_tests.cpp
@@ -18,7 +18,13 @@ public:
 };
 
 TEST_F(Lab04Fixture, build_test){
-    std::string test = "8/2";
+    std::string test = "4+2+5+8";
+    lab4::calculator obj(test);
+    EXPECT_EQ(19, obj.calculate());
+}
+
+TEST_F(Lab04Fixture, complex_eq){
+    std::string test = "2-(3*4/6)+4";
     lab4::calculator obj(test);
     EXPECT_EQ(4, obj.calculate());
 }
